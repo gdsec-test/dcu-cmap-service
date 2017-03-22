de9914705fab7028250f5d10e4ffd7e600ed128a
Chris Hauser <chauser@godaddy.com>
2017-03-21 18:37:34 -0700

    Added fix for resolv.conf issues on k8s

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+python -c "import re;f=open('/etc/resolv.conf','r');t=f.read();t=re.sub(r'ndots:\d+',r'ndots:1',t);open('/etc/resolv.conf','w').write(t)"
+
 if [ -z "$HTTP_ONLY" ]
 then
     exec /usr/bin/uwsgi --ini /app/uwsgi_ssl.ini --need-app
