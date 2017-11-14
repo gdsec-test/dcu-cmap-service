a71d88148664aafdfb069a7825ee4f871fb72bc1
bxberry <bxberry@godaddy.com>
2017-11-14 10:11:21 -0700

    Added conditional so nameserver is only appended if nameserver isnt found in file contents

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-python -c "import re;f=open('/etc/resolv.conf','r');t=f.read();t=re.sub(r'(nameserver \S+)',r'\1\nnameserver 172.31.251.11',t);t=re.sub(r'ndots:\d+',r'ndots:1',t);open('/etc/resolv.conf','w').write(t)"
+python -c "import re;ns='172.31.251.11';f=open('/etc/resolv.conf','r');t=f.read();regexp=re.compile(r'{}'.format(ns));t=re.sub(r'(nameserver \S+)',r'\1\nnameserver {}'.format(ns),t) if not regexp.search(t) else t;t=re.sub(r'ndots:\d+',r'ndots:1',t);open('/etc/resolv.conf','w').write(t)"
 
 if [ -z "$HTTP_ONLY" ]
 then
