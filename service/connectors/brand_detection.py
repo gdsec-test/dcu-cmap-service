8114283cd95216ce839cc70f7a291564ad596f22
Lance Halvorson <lhalvorson@godaddy.com>
2019-11-14 12:42:29 -0700

    [CMAPT-3212] CMAP Service Auth to Branddetection (#112)
    * Adds Auth for CMAP Service to Brand Detection calls
    
    * Adding headers to request to branddetection
    
    * Change request headers to include data without update
    
    * Fix tab

--- service/connectors/brand_detection.py
+++ service/connectors/brand_detection.py
@@ -1,6 +1,12 @@
+import json
 import logging
+import os
 
 import requests
+
+from settings import config_by_name
+
+config = config_by_name[os.getenv('sysenv', 'dev')]()
 
 
 class BrandDetectionHelper(object):
@@ -15,6 +21,10 @@ class BrandDetectionHelper(object):
         self._hosting_endpoint = '{}/hosting?domain={{}}'.format(url)
         self._registrar_endpoint = '{}/registrar?domain={{}}'.format(url)
 
+        self._sso_endpoint = config.SSO_URL + '/v1/secure/api/token'
+        cert = (config.CMAP_SERVICE_CERT, config.CMAP_SERVICE_KEY)
+        self._headers = {'Content-Type': 'application/json', 'Authorization': 'sso-jwt {}'.format(self._get_jwt(cert))}
+
     def get_hosting_info(self, domain):
         """
         Attempt to retrieve the hosting information from the Brand Detection service
@@ -24,7 +34,7 @@ class BrandDetectionHelper(object):
         self._logger.info('Fetching hosting information for {}'.format(domain))
 
         try:
-            return requests.get(self._hosting_endpoint.format(domain)).json()
+            return requests.get(self._hosting_endpoint.format(domain), headers=self._headers).json()
         except Exception as e:
             self._logger.error('Unable to query Brand Detection service for {} : {}'.format(domain, e))
             return {'brand': None, 'hosting_company_name': None, 'hosting_abuse_email': None, 'ip': None}
@@ -38,8 +48,24 @@ class BrandDetectionHelper(object):
         self._logger.info('Fetching registrar information for {}'.format(domain))
 
         try:
-            return requests.get(self._registrar_endpoint.format(domain)).json()
+            return requests.get(self._registrar_endpoint.format(domain), headers=self._headers).json()
         except Exception as e:
             self._logger.error('Unable to query Brand Detection service for {} : {}'.format(domain, e))
             return {'brand': None, 'registrar_name': None, 'registrar_abuse_email': None, 'domain_create_date': None,
                     'domain_id': None}
+
+    def _get_jwt(self, cert):
+        """
+        Attempt to retrieve the JWT associated with the cert/key pair from SSO
+        :param cert:
+        :return: jwt
+        """
+        try:
+            response = requests.post(self._sso_endpoint, data={'realm': 'cert'}, cert=cert)
+            response.raise_for_status()
+
+            body = json.loads(response.text)
+            return body.get('data')  # {'type': 'signed-jwt', 'id': 'XXX', 'code': 1, 'message': 'Success', 'data': JWT}
+        except Exception as e:
+            self._logger.error(e.message)
+        return
