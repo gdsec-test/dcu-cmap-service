532e6273d88ed46eccf2d5b7480c3ec650eb8df4
Brett Berry <bxberry@godaddy.com>
2020-01-06 13:48:06 -0700

    [cmapt-3316] General cleanup, move a few private methods to static, removed the response_key arg from soap call as it wasnt being used (#120)

--- service/connectors/smdb.py
+++ service/connectors/smdb.py
@@ -7,9 +7,9 @@ from zeep.transports import Transport
 
 
 class Ipam(object):
-    '''
+    """
     This class provides access into the IPAM Soap Service to retrieve and modify data about IP Addresses
-    '''
+    """
 
     def __init__(self, url, user, password):
         self._logger = logging.getLogger(__name__)
@@ -21,7 +21,8 @@ class Ipam(object):
 
         self.client = Client(url, transport=transport)
 
-    def __get_ips(self, obj):
+    @staticmethod
+    def __get_ips(obj):
         """
         Get a list of IP Addresses from an object
         :param obj:
@@ -35,14 +36,13 @@ class Ipam(object):
 
         return ips
 
-    def __soap_call(self, method, params, response_key):
-        '''
+    def __soap_call(self, method, params):
+        """
         Perform a SOAP call, and parse the results.
         :param method:
         :param params:
-        :param response_key:
         :return:
-        '''
+        """
 
         # We need the params to be a tuple, so if it's a single element, cast as a tuple.
         if not isinstance(params, tuple):
@@ -54,31 +54,31 @@ class Ipam(object):
         except Exception as e:
             self._logger.error('Unable to make SOAP call: {}'.format(e))
 
-    def __validate_params(self, params):
-        '''
+    @staticmethod
+    def __validate_params(params):
+        """
         Make sure that all method parameters were supplied.
         :param params:
         :return:
-        '''
+        """
         for key, val in params.items():
             if not val:
                 raise Exception('Missing parameter {}'.format(key))
 
     def get_ips_by_hostname(self, hostname):
-        '''
+        """
         Get IP's assigned to a specific hostname. Returns a list.
         :param hostname:
         :return:
-        '''
-        self.__validate_params(locals())
-        s = self.__soap_call('GetIPsByHostname', hostname, 'GetIPsByHostnameResult')
-        return self.__get_ips(self.__soap_call('GetIPsByHostname', hostname, 'GetIPsByHostnameResult'))
+        """
+        Ipam.__validate_params(locals())
+        return Ipam.__get_ips(self.__soap_call('GetIPsByHostname', hostname))
 
     def get_properties_for_ip(self, ip):
-        '''
+        """
         Get details for a specific IP address. Returns a dictionary.
         :param ip:
         :return:
-        '''
-        self.__validate_params(locals())
-        return self.__soap_call('GetPropertiesForIP', ip, 'GetPropertiesForIPResult')
+        """
+        Ipam.__validate_params(locals())
+        return self.__soap_call('GetPropertiesForIP', ip)
