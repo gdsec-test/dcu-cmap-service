703b69fae518683d29fb686868faded976a00abf
Brett Berry <bxberry@godaddy.com>
2020-11-13 16:05:19 -0700

    [CMAPT-4089] Update ip for devphishstory db
    Using new `mongodb-dev` VM

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ class OTEAppConfig(AppConfig):
 
 class DevelopmentAppConfig(AppConfig):
     DB = 'devphishstory'
-    DB_HOST = '10.22.188.208'
+    DB_HOST = '10.36.156.188'
     DB_USER = 'devuser'
 
     SSO_URL = 'https://sso.dev-godaddy.com'
