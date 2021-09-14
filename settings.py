f0378b5821e50db4ce464681214d18d03776c0c1
nwade-godaddy <80975008+nwade-godaddy@users.noreply.github.com>
2021-09-13 20:45:23 -0500

    Use non-compromised mongo credentials (#12)

--- settings.py
+++ settings.py
@@ -71,7 +71,7 @@ class AppConfig(object):
 class ProductionAppConfig(AppConfig):
     DB = 'phishstory'
     DB_HOST = '10.22.9.209'
-    DB_USER = 'sau_p_phish'
+    DB_USER = 'sau_p_phishv2'
 
     SSO_URL = 'https://sso.godaddy.com'
 
