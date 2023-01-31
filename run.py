d30a8ecfcb00717c6b0b154ad81e3d939024ea99
htracey-godaddy <htracey@godaddy.com>
2023-01-31 12:14:05 -0800

    adding 422

--- run.py
+++ run.py
@@ -111,7 +111,7 @@ def lookup_product_entitlements(customerId, entitlementId):
     for entitlement in entitlements:
         product = entitlement.get("product")
         if product == "Diablo WHMCS" or product == "Plesk":
-            return {'message': f'{product} has not been implemented yet'}, 500
+            return {'message': f'{product} has not been implemented yet'}, 422
         results.append(resolver.locate_product(entitlement.get("domain"), entitlementId, "", product))
     return json.dumps(results), 200
 
